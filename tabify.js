dc3147cec6f19e10e61fea966b637e5f2418b37f
Kyle Schaeffer <kyle@oldrivercreative.com>
2015-05-05 16:33:49 -0400

    Initial upload and testing

--- tabify.js
+++ tabify.js
@@ -1,72 +1,129 @@
-/*! tabify.js v1.0 | MIT License | https://github.com/oldrivercreative/wptabify.js */
+/*! tabify.js v1.0 | MIT License | https://github.com/oldrivercreative/tabify.js */
 (function($){
     $.fn.tabify = function(options){
         
         // settings
         var settings = $.extend(true, {}, {
             selector: this.selector,
-            disabled: $('#MSOLayout_InDesignMode').val() == 0 ? false : true,
+            enabled: $('#MSOLayout_InDesignMode').val() != '1' ? true : false,
             panel: '.ms-webpartzone-cell',
             container: '.ms-webpart-zone',
             title: 'h2.ms-webpart-titleText:eq(0)',
+            hidetitles: false,
             cssclass: 'ui-tabs',
+            reversez: false,
             oninit: false,
             onchange: false,
             destroy: false
         }, options);
         
-        // wptabify
+        // tabify
         this.each(function(){
             
             // get objects
-            var panel = $(this).closest(settings.panel);
-            var container = part.closest(settings.container);
+            var trigger = $(this).closest(settings.panel);
+            var container = trigger.closest(settings.container);
             
             // destroy?
             if(settings.destroy){
-                container.trigger('tabs-destroy');
+                container.trigger('tabify-destroy');
             }
             
             // create
             else {
                 
                 // init
-                container.on('tabs-init', function(){
+                container.on('tabify-init', function(){
 
                     // add classes
                     container.addClass(settings.cssclass);
                     
-                    // tabs
+                    // <ul>
                     var ul = $('<ul class="' + settings.cssclass + '-nav" />');
                     
-                    // create tabs
-                    zone.children(settings.partselector).each(function(i){
+                    // panels
+                    container.find(settings.panel).each(function(i){
                         
-                        // get cell
-                        var cell = $(this);
+                        // get panel
+                        var panel = $(this);
                         
-                        // not tabify cell?
-                        if(cell.find(settings.selector).size() == 0){
+                        // not trigger?
+                        if(!panel.is(trigger)){
+                            
+                            // add classes
+                            panel.addClass(settings.cssclass + '-panel');
                             
                             // id
-                            var id = 'wp-section' + i;
-                            cell.attr('id', id);
+                            var id = 'panel' + (i + 1);
+                            panel.attr('id', id);
                             
                             // title
-                            var titletext = 'No title';
-                            var title = cell.find(settings.titleselector);
+                            var label = 'No title';
+                            var title = panel.find(settings.title);
                             
-                            // found title
+                            // found title?
                             if(title.size() > 0){
-                                titletext = title.text();
+                                
+                                // title text?
+                                var titletext = title.text();
+                                if(titletext.length > 0){
+                                    label = titletext;
+                                }
+                                
+                                // hide title?
+                                if(settings.hidetitles){
+                                    title.addClass(settings.cssclass + '-hidden');
+                                }
+                                
                             }
                             
-                            // create link
-                            ul.append('<li><a href="#' + id + '">' + titletext + '</a></li>');
+                            // <li>
+                            var li = $('<li><a href="#' + id + '"><span>' + label + '</span></a></li>');
+                            
+                            // <a>
+                            li.children('a').on('click', function(e){
+                                
+                                // prevent hash in url
+                                e.preventDefault();
+                                
+                                // target
+                                var target = $(this).attr('href');
+                                
+                                // change
+                                container.trigger('tabify-change', [ target ]);
+                                
+                            });
+                            
+                            // active panel?
+                            if(i == 0){
+                                panel.addClass('active');
+                                li.addClass('active');
+                            }
+                            
+                            // add to <ul>
+                            ul.append(li);
                             
                         }
                         
+                        // trigger
+                        else{
+                            panel.addClass(settings.cssclass + '-hidden');
+                        }
+                        
                     });
+                    
+                    // reverse z-index?
+                    if(settings.reversez){
+                        
+                        // get <li>'s
+                        var items = ul.children('li');
+                        
+                        // z-index
+                        items.each(function(i){
+                            $(this).css('z-index', items.size() - i);
+                        });
+                        
+                    }
                     
                     // add tabs
                     container.prepend(ul);
@@ -78,25 +135,42 @@
 
                 });
                 
+                // change
+                container.on('tabify-change', function(e, target){
+                    
+                    // deactivate links
+                    container.children('ul:eq(0)').children('li').removeClass('active');
+                    
+                    // deactivate panels
+                    container.find(settings.panel).removeClass('active');
+                    
+                    // activate link
+                    container.find('a[href="' + target + '"]').parent('li').addClass('active');
+                    
+                    // activate panel
+                    $(target).addClass('active');
+                    
+                });
+                
                 // destroy
-                container.on('tabs-destroy', function(){
+                container.on('tabify-destroy', function(){
                     
                     // remove classes
                     container.removeClass(settings.cssclass);
                     
-                    // remove ids
-                    container.find(settings.panel).removeAttr('id');
+                    // remove panels
+                    container.find(settings.panel).removeAttr('id').removeClass(settings.cssclass + '-panel active');
                     
-                    // remove tabs
-                    container.children('ul.ui-tabs').remove();
+                    // remove <ul>
+                    container.children('ul:eq(0)').remove();
                     
                 });
 
-                // not in design mode?
-                if(settings.editmode){
+                // enabled?
+                if(settings.enabled){
                     
                     // init
-                    zone.trigger('tabs-init');
+                    container.trigger('tabify-init');
                     
                 }
                 
